2dbf6b4aa5e6633285a5d098f6812ff1c533cfc3
Cyril Chable <cyril.chable@outlook.be>
2023-01-26 17:07:12 +0100

    Added      http/module/Transport/src/Service/IndisponibiliteVehiculeManager.php Added      http/module/Transport/src/Form/IndisponibiliteVehiculeForm.php

---
+++ http/module/Transport/src/Form/IndisponibiliteVehiculeForm.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Form for the encoding an IndisponibiliteVehicule
+ * 
+ * @package   module/Transport/src/Form/IndisponibiliteVehicule.php
+ * @version   1.0
+ * @copyright 2018-23 H.P.B
+ * @author    Marsh <cyril.chable@outlook.be>
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Transport\Form;
+
+use DomainException;
+
+use Laminas\Form\Form;
+use Laminas\Form\Element;
+use Laminas\Form\Element\Time;
+use Laminas\Form\Element\Checkbox;
+
+use Laminas\Filter\StringTrim;
+use Laminas\Filter\StripTags;
+use Laminas\Filter\ToInt;
+use Laminas\Filter\DateTimeFormatter;
+
+use Laminas\InputFilter\InputFilter;
+use Laminas\InputFilter\InputFilterAwareInterface;
+use Laminas\InputFilter\InputFilterInterface;
+
+use Laminas\Validator\StringLength;
+
+use Transport\Model\IndisponibiliteVehicule;
+
+
+/*
+ * 
+ */
+class IndisponibiliteVehiculeForm extends Form
+{
+
+  /*
+   * Scenario ('create' or 'update').
+   * @var string 
+   */
+  private $scenario;
+
+  /**
+   * haystackVehicules : liste of vehicules
+   * @var array
+   */
+  private $haystackVehicules;
+
+  /**
+   * Constructor
+   */
+  public function __construct(array $haystackVehicules)
+  {
+
+    // Save parameters for internal use
+    $this->haystackVehicules = $haystackVehicules;
+      
+    // Define form name
+    parent::__construct('indisponibilitechauffeur-form');
+    
+    // Set POST method for this form
+    $this->setAttribute('method', 'post');
+    
+    $this->addElements();
+    $this->addInputFilter();
+  }
+  
+  /*
+   * This method adds elements to form (input fields and submit button).
+   */
+  protected function addElements() 
+  {
+    
+    // Add "idxVehicule" field
+    $this->add([
+      'name' => 'IDX_VEHICULE',
+      'type' => 'select',
+      'options' => [
+        'value_options' => $this->haystackVehicules,
+        'label'         => 'Vehicule',
+      ],
+    ]);
+    
+    // Add "start date" field
+    $this->add([
+      'name' => 'STARTDATEINDISPONIBILITE',
+      'type' => 'date',
+      'options' => [
+        'label' => 'Date de début',
+      ],
+    ]);
+
+    // Add "start time" field
+    $this->add([
+      'name'    => 'STARTTIMEINDISPONIBILITE',
+      'type'    => Time::class,
+      'options' => [
+        'label'  => 'Heure début',
+        'format' => 'H:i:s',
+      ],
+      'attributes' => [
+        'min'  => '00:00:00',
+        'max'  => '23:59:59',
+        'step' => '1',
+      ],
+    ]);
+    
+    // Add "end date" field
+    $this->add([
+      'name' => 'ENDDATEINDISPONIBILITE',
+      'type' => 'date',
+      'options' => [
+        'label' => 'Date de fin',
+      ],
+    ]);
+    
+    // Add "end time" field
+    $this->add([
+      'name'    => 'ENDTIMEINDISPONIBILITE',
+      'type'    => Time::class,
+      'options' => [
+        'label'  => 'Heure fin',
+        'format' => 'H:i:s',
+      ],
+      'attributes' => [
+        'min'  => '00:00:00',
+        'max'  => '23:59:59',
+        'step' => '1',
+      ],
+    ]);
+      
+    // Add "all days" field
+    $this->add([
+      'name'  => 'ALLDAYINDISPONIBILITE',
+      'type'  => Checkbox::class,
+        
+      'attributes' => [
+        'id'    => 'ALLDAYINDISPONIBILITE',
+        'class' => 'form-check-input',
+        'value' => '0',
+      ],
+        
+      'options' => [
+        'use_hidden_element' => true,
+        'checked_value'      => '1',
+        'unchecked_value'    => '0',
+        'label'              => 'Toute la journée',
+        'label_attributes'   => [
+          'for' => 'ALLDAYINDISPONIBILITE',
+        ],
+      ],
+    ]);
+
+    // Add the Submit button
+    $this->add([
+      'name'       => 'submit',
+      'type'       => 'submit',
+      'attributes' => [
+        'value' => 'Sauver',
+        'id'    => 'submit',
+      ],
+    ]);
+    
+    // Add the CSRF field
+    $this->add([
+      'name'    => 'csrf',
+      'type'    => 'csrf',
+      'options' => [
+        'csrf_options' => [
+          'timeout' => 600
+        ]
+      ],
+    ]);
+  }
+  
+  /**
+   * This method creates input filter (used for form filtering/validation).
+   */
+  private function addInputFilter() 
+  {
+    
+    // Create input filter
+    $inputFilter = $this->getInputFilter();
+
+    $indisponibiliteVehicule = new IndisponibiliteVehicule();
+    $indisponibiliteVehicule->fillInputFilter($inputFilter);
+  }
+}
---
+++ http/module/Transport/src/Service/IndisponibiliteVehiculeManager.php
@@ -0,0 +1,218 @@
+<?php
+/**
+ * This service is responsible for add/edit/delete 'indisponibilite vehicule'. 
+ *
+ * @package   module/Transport/src/Service/IndisponibiliteVehiculeManager.php
+ * @version   1.0
+ * @copyright 2018-23 H.P.B
+ * @author    Marsh <cyril.chable@outlook.be>
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ **/
+
+namespace Transport\Service;
+
+use Transport\Model\IndisponibiliteVehicule;
+use Transport\Model\IndisponibiliteVehiculeTable;
+use Transport\Model\VehiculeTable;
+
+
+/**
+ * 
+ */
+class IndisponibiliteVehiculeManager
+{
+  
+  /**
+   * IndisponibiliteVehicule table manager.
+   * @var Transport\Model\INdisponibiliteVehiculeTable
+   */
+  private $indisponibiliteVehiculeTable;
+  
+  /**
+   * Vehicule table manager.
+   * @var Transport\Model\VehiculeTable
+   */
+  private $vehiculeTable;
+  
+  /**
+   * PHP template renderer.
+   * @var type 
+   */
+  private $viewRenderer;
+
+  /**
+   * Application config.
+   * @var type 
+   */
+  private $config;
+
+  
+  /*
+   * Constructs the service.
+   */
+  public function __construct(IndisponibiliteVehiculeTable $indisponibiliteVehiculeTable, VehiculeTable $vehiculeTable, $viewRenderer, $config) 
+  {
+    
+    $this->indisponibiliteVehiculeTable = $indisponibiliteVehiculeTable;
+    $this->vehiculeTable                = $vehiculeTable;
+    $this->viewRenderer = $viewRenderer;
+    $this->config       = $config;
+  }
+    
+  /*
+   * This method adds a new vehicule.
+   */
+  public function addIndisponibiliteVehicule($data) 
+  {
+    
+    //Clean $data from FROM
+    unset($data['csrf']);
+    unset($data['submit']);
+    
+    if(!$this->checkIndisponibiliteVehiculeExists($data)) {
+    //if(!$this->indisponibiliteVehiculeTable->findOneByRecord($data)) {
+      
+      // Create new IndisponibiliteVehicule entity.
+      $indisponibiliteVehicule= new IndisponibiliteVehicule();
+      $indisponibiliteVehicule->exchangeArray($data);
+      
+      // Check if Vehicule exist
+      $idVehicule = $data['IDX_VEHICULE'];
+      $vehicule = $this->vehiculeTable->findOneById($idVehicule);
+      if ($vehicule == null) {
+        
+        throw new \Exception('Vehicule not found');
+      }
+      
+      return $this->indisponibiliteVehiculeTable->saveIndisponibiliteVehicule($indisponibiliteVehicule);
+      }
+    
+    return false;
+  }
+    
+  /*
+   * This method update datas of an existing indisponibilitevehicule
+   */
+  public function updateIndisponibiliteVehicule($indisponibiliteVehicule, $data) 
+  {
+    
+    // Do not allow to change indisponibilitevehicule if another indisponibilitevehicule with such data already exits
+    if($this->checkIndisponibiliteVehiculeExists($data)) {
+ 
+      return false;
+    }
+    
+    //find vehicule id
+    $idVehicule = $data['IDX_VEHICULE'];
+    $vehicule = $this->vehiculeTable->findOneById($idVehicule);
+    if($vehicule == null) {
+      
+      throw new \Exception('Vehicule not found');
+    }
+    
+    $indisponibiliteVehicule->exchangeArray($data, false);
+
+    // Apply changes to database.
+    $this->indisponibiliteVehiculeTable->saveIndisponibiliteVehicule($indisponibiliteVehicule);
+    
+    return $indisponibiliteVehicule;
+  }
+  
+  /**
+   * Deletes the given vehicule.
+   */
+  public function deleteIndisponibiliteVehicule($id)
+  {
+    
+    $this->indisponibiliteVehiculeTable->deleteIndisponibiliteVehicule($id);
+  }
+
+  /*
+   *
+   */
+  public function checkIndisponibiliteVehiculeExists(array $data) 
+  {
+
+    $search['IDX_VEHICULE']            = $data['IDX_VEHICULE'];
+    $search['STARTDATEINDISPONIBILITE'] = $data['STARTDATEINDISPONIBILITE'];
+    $indisponibiliteVehicule = $this->indisponibiliteVehiculeTable->findOneBy($search);
+    $bResult = false;
+    if($indisponibiliteVehicule) {
+      
+      if($indisponibiliteVehicule->getJourEntier() || $data['ALLDAYINDISPONIBILITE']) {
+        
+        $bResult = true;
+      } else {
+        
+        if($indisponibiliteVehicule->getDateFin() != $data['ENDDATEINDISPONIBILITE']) {
+          
+          $bResult = true;
+        } else {
+          
+          if (checkTimeBetween(
+                $indisponibiliteVehicule->getHeureDebut(), 
+                $data['STARTTIMEINDISPONIBILITE'], 
+                $data['ENDTIMEINDISPONIBILITE']
+                )
+              ||
+              checkTimeBetween(
+                $indisponibiliteVehicule->getHeurefin(),
+                $data['STARTTIMEINDISPONIBILITE'], 
+                $data['ENDTIMEINDISPONIBILITE']
+                )
+             ) {
+                
+            $bResult = true;
+          } else {      
+            $bResult = checkRangeTimeBetween(
+              $indisponibiliteVehicule->getHeureDebut(), 
+              $indisponibiliteVehicule->getHeureFin(), 
+              $data['STARTTIMEINDISPONIBILITE'], 
+              $data['ENDTIMEINDISPONIBILITE']
+            );
+          }
+        }
+      }
+    }
+    
+    return $bResult;
+  }
+  
+  private function checkTimeBetween($value, $start, $end) 
+  {
+    
+    $timeValue = new DateTime($value);
+    $timeStart = new DateTime($start);
+    $timeEnd   = new DateTime($end);
+    
+    return (($timeValue > $timeStart) && ($timeValue < $timeEnd));  
+  }
+  
+  private function checkRangeTimeBetween($start1, $end1, $start2, $end2) {
+    
+    $timeStart1 = new DateTime($start1);
+    $timeEnd1   = new DateTime($end1);
+    $timeStart2 = new DateTime($start2);
+    $timeEnd2   = new DateTime($end2);
+
+    return (($timeStart1 > $timeStart2) && ($timeEnd1 < $timeEnd2) || ($timeStart1 < $timeStart2) && ($timeEnd1 > $timeEnd2));
+  }
+  
+  /*
+   * 
+   */
+  public function getVehicules() 
+  {
+    
+    $vehiculesList = [];
+    
+    $vehicules = $this->vehiculeTable->fetchAll();
+
+    foreach ($vehicules as $vehicule) {
+      $vehiculesList[$vehicule->getId()] = $vehicule->getNom();
+    }
+    
+    return $vehiculesList;
+  }  
+}
+
